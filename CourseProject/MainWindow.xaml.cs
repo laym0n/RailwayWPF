35378347f3103f971b654ee98a1a017ef805e8c0
victor <victor@LAYM0N>
2022-11-19 17:11:04 +0300

    Добавьте файлы проекта.

---
+++ CourseProject/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CourseProject
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Main.Navigate(new Page1());
+        }
+
+        private void load_main_page()
+        {
+            
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Navigate(new Page2());
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            Main.Navigate(new Page1());
+        }
+    }
+}
