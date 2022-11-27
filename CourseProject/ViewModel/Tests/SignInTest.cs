db1cc94a4384d44710faf96781f4d50d1f65101f
victor <victor@LAYM0N>
2022-11-27 15:14:56 +0300

    Создал папки для MVVM

---
+++ CourseProject/ViewModel/Tests/SignInTest.cs
@@ -0,0 +1,21 @@
+﻿using CourseProject.ViewModel.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseProject.ViewModel.Tests
+{
+    internal class SignInTest : ISignIn
+    {
+        public SignInTest()
+        {
+
+        }
+        public bool SignIn()
+        {
+            return true;
+        }
+    }
+}
