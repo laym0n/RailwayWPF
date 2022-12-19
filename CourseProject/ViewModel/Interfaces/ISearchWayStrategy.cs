a2a7dfe338782d49f5bd3fce68873b1702108f29
victor <victor@LAYM0N>
2022-12-19 22:17:37 +0300

    Начинаю добавлять паттерн Стратегия для поиска пути

---
+++ CourseProject/ViewModel/Interfaces/ISearchWayStrategy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CourseProject.Model;
+using CourseProject.Model.ModelsForGetInfoFromView;
+
+namespace CourseProject.ViewModel.Interfaces
+{
+    public interface ISearchWayStrategy
+    {
+        List<ConcreteWayFromStationToStation> FindWays(InfoAboutSearchingWaysModel info);
+    }
+}
---
+++ CourseProject/ViewModel/StrategiesSearchWay/SearchWayStrategyWithLessOrEqualSixTransfer.cs
@@ -0,0 +1,20 @@
+﻿using CourseProject.Model.ModelsForGetInfoFromView;
+using CourseProject.Model;
+using CourseProject.ViewModel.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseProject.ViewModel.StrategiesSearchWay
+{
+    public class SearchWayStrategyWithLessOrEqualSixTransfer : ISearchWayStrategy
+    {
+
+        public List<ConcreteWayFromStationToStation> FindWays(InfoAboutSearchingWaysModel info)
+        {
+            return null;
+        }
+    }
+}
